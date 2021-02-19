4d2b876e63b506fc74ee43aa8860e0d81fd8bc16
Santhosh136 <snthosh602@gmail.com>
2021-02-19 21:23:14 +0530

    first commit

---
+++ src/main/java/gct/it/computerlabmonitoring/ComputerLabMonitoringApplication.java
@@ -0,0 +1,12 @@
+package gct.it.computerlabmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerLabMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerLabMonitoringApplication.class, args);
+	}
+}
---
+++ src/test/java/gct/it/computerlabmonitoring/ComputerLabMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package gct.it.computerlabmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerLabMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
